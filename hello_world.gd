7861d3c705b3e8a178791a06fcb554392a67be8a
R2Bleep2 <126963542+R2Bleep2@users.noreply.github.com>
2025-04-07 16:36:38 +0100

    Update hello_world.gd

--- hello_world.gd
+++ hello_world.gd
@@ -1,2 +1,5 @@
-## The "Hello, world!" program in GDScript.
-print('Hello, world!')
+## A Node that outputs "Hello, world!" to Godot's console when instantiated in a scene.
+extends Node
+
+func _ready():
+	print("Hello, world!")
